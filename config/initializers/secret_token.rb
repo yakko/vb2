0db10973132bfc028f78208fca379adc03a4e170
Thiago Almeida <tapgyn@gmail.com>
2011-12-24 00:41:26 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vb2::Application.config.secret_token = '8d48ebdb91463e0202223f5e26222c2016058a1674975e9d4e4e6d0bd38a7a7b965d5a9615204bb8a05955850d9ec29f82c6083f2f83c22fa34a4ea2f0ebf44f'
